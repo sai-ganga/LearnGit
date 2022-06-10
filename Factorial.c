bb31baa84ccc11db6fb5865f4417d5656d5c1af1
sai-ganga <107107133+sai-ganga@users.noreply.github.com>
2022-06-10 11:37:21 +0530

    Update Factorial.c

---
+++ Factorial.c
@@ -0,0 +1,11 @@
+include<stdio.h>
+
+int factorial(int n);
+int main () {
+int factorial(int n) {
+    if (n == 1 || n == 0) {
+    return 1;
+    } else {
+    return n " factorial(n - 1);
+    }
+    
